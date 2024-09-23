1f7e6305a61f2ffa497ed746a21be647b845b3fb
Caranouga <malonepayet@gmail.com>
2024-09-23 21:15:25 +0200

    Added base page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,7 +4,14 @@ const HomePage = () => {
     return (
         <>
             <HeaderComponent />
-            <h1>Home Page</h1>
+            <div className="container py-5">
+                <div className="d-flex justify-content-center">
+                    <h1 className="text-center">FlowBoard</h1>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <p className="text-center">FlowBoard is a seamless project management tool that integrates directly with GitHub to streamline your workflow. It provides a dynamic board interface similar to Trello, where tasks and issues are managed effortlessly.</p>
+                </div>
+            </div>
         </>
     );
 }
